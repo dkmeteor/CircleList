c7ec7d1886fc43b43dfdf99e7ca170597a43f26c
DK <93440331@qq.com>
2016-02-29 00:04:00 +0800

    refine code

--- app/src/main/java/com/dk/ui/circlelist/MainActivity.java
+++ app/src/main/java/com/dk/ui/circlelist/MainActivity.java
@@ -15,24 +15,18 @@ import android.widget.AbsListView.OnScrollListener;
 
 public class MainActivity extends Activity {
 
-    private ListView lv;
-    private int[] images = new int[] { R.drawable.p1, R.drawable.p2, R.drawable.p3 };
+    private ListView listview;
+    private int[] images = new int[]{R.drawable.p1, R.drawable.p2, R.drawable.p3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv = (ListView)findViewById(R.id.lv);
-        lv.setAdapter(new MyAdapter());
-        lv.setClipToPadding(false);
-        lv.setClipChildren(false);
-//        try {
-//            changeGroupFlag(lv);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        lv.setOnScrollListener(new OnScrollListener() {
+        listview = (ListView) findViewById(R.id.lv);
+        listview.setAdapter(new MyAdapter());
+        listview.setClipToPadding(false);
+        listview.setClipChildren(false);
+        listview.setOnScrollListener(new OnScrollListener() {
 
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -41,8 +35,8 @@ public class MainActivity extends Activity {
 
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                for (int i = 0; i < lv.getChildCount(); i++) {
-                    lv.getChildAt(i).invalidate();
+                for (int i = 0; i < listview.getChildCount(); i++) {
+                    listview.getChildAt(i).invalidate();
                 }
             }
         });
@@ -68,11 +62,11 @@ public class MainActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                MatrixView m = (MatrixView)LayoutInflater.from(MainActivity.this).inflate(R.layout.view_list_item, null);
-                m.setParentHeight(lv.getHeight());
+                MatrixView m = (MatrixView) LayoutInflater.from(MainActivity.this).inflate(R.layout.view_list_item, null);
+                m.setParentHeight(listview.getHeight());
                 convertView = m;
             }
-            ImageView imageView = (ImageView)convertView.findViewById(R.id.image);
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.image);
             imageView.setImageResource(images[position % images.length]);
 
             return convertView;
@@ -80,14 +74,13 @@ public class MainActivity extends Activity {
 
     }
 
-    public void changeGroupFlag(Object obj) throws Exception
-    {
+    @Deprecated
+    public void changeGroupFlag(Object obj) throws Exception {
         Field[] f = obj.getClass().getSuperclass().getSuperclass().getSuperclass().getDeclaredFields(); // 获得成员映射数组
-        for (Field tem : f) 
-        {
+        for (Field tem : f) {
             if (tem.getName().equals("mGroupFlags")) {
                 tem.setAccessible(true);
-                Integer mGroupFlags = (Integer)tem.get(obj); 
+                Integer mGroupFlags = (Integer) tem.get(obj);
                 int newGroupFlags = mGroupFlags & 0xfffff8;
                 tem.set(obj, newGroupFlags);
             }
